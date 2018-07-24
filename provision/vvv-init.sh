deda82e80c64c83fcd96f69639bf7631e9c6b589
Ryan Leeson <rleeson@trellist.com>
2018-07-24 11:55:44 -0400

    First update adds more configuration for setting up WP and adds pulling WPEngine related sites

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 # Provision WordPress Stable
 
+# Get the value of a key for WPEngine setups
+# @param ${1} - Key to find
+# @param ${2} - Optional default value
+get_wpengine_value() {
+  local value=`cat ${VVV_CONFIG} | shyaml get-value sites.${SITE_ESCAPED}.custom.wpengine.${1} 2> /dev/null`
+  echo ${value:-$2}
+}
+
+# Standard configuration variables
 DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
 DOMAINS=`get_hosts "${DOMAIN}"`
 SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
@@ -8,6 +17,15 @@ WP_VERSION=`get_config_value 'wp_version' 'latest'`
 WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+
+# Optional Wordpress default user values
+WP_ADMIN_EMAIL=`get_config_value 'wp_admin_email' 'admin@local.test'`
+WP_ADMIN_USER=`get_config_value 'wp_admin_user' 'admin'`
+WP_ADMIN_PASS=`get_config_value 'wp_admin_pass' 'password'`
+
+# Choose different hosting types, allows the setup of different site types
+# Accepted types are 'self', 'wpengine', 'vip'
+WP_HOST_TYPE=`get_config_value 'wp_host_type' 'self'`
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
@@ -20,12 +38,34 @@ mkdir -p ${VVV_PATH_TO_SITE}/log
 touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
-# Install and configure the latest stable version of WordPress
+# WPEngine sites user repositories installed at the site root, pull the site repo first
+if [ "wpengine" == "${WP_HOST_TYPE}" ]; then
+  WPENGINE_REPO = `get_wpengine_value 'repo' ''`
+  cd ${VVV_PATH_TO_SITE}/public_html
+  
+  if [[ "${WPENGINE_REPO}" ]] && [[ "public_html/" == "$(git rev-parse --show-prefix)" ]]; then
+    echo -e "Cloning WPEngine compatible site repository from ${WPENGINE_REPO}"
+    git clone ${WPENGINE_REPO} --quiet
+  else
+    if [ -n "$(git diff-index --quiet HEAD --)" ]; then
+      echo -e "Updating clean branch $(git rev-parse --abbrev-ref HEAD) from ${WPENGINE_REPO}"      
+      git pull --quiet
+    else
+      echo -e "Branch $(git rev-parse --abbrev-ref HEAD) has working copy changes, no update"
+    fi
+  fi
+fi
+
+# Make sure we are in the site directory before installing/modifying WordPress
+cd ${VVV_PATH_TO_SITE}/public_html
+
+# Ensure the requested version of WordPress is downloaded to the site directory
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
-    echo "Downloading WordPress..."
+  echo "Downloading WordPress..."
 	noroot wp core download --version="${WP_VERSION}"
 fi
 
+# Add a configuration file if one is missing
 if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
   echo "Configuring WordPress Stable..."
   noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
@@ -33,7 +73,9 @@ define( 'WP_DEBUG', true );
 PHP
 fi
 
-if ! $(noroot wp core is-installed); then
+# New installs will choose the correct install type from WP_TYPE
+# Existing installs will be updated for the requested version
+if [[ ! $(noroot wp core is-installed) ]]; then
   echo "Installing WordPress Stable..."
 
   if [ "${WP_TYPE}" = "subdomain" ]; then
@@ -44,16 +86,18 @@ if ! $(noroot wp core is-installed); then
     INSTALL_COMMAND="install"
   fi
 
-  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.test" --admin_password="password"
+  noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=${WP_ADMIN_USER} --admin_email="${WP_ADMIN_EMAIL}" --admin_password="${WP_ADMIN_PASS}"
 else
   echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html
+
   noroot wp core update --version="${WP_VERSION}"
 fi
 
+# Add/replace the Nginx site configuration for all site domains
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 
+# Add TLS certificates for the environment, either generic or via the TLS CA utility
 if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
     sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
     sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
