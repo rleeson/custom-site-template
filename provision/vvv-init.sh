502d9e4d3c16d7ba062645a1b48dd36dafe4a8ce
Ryan Leeson <rleeson@trellist.com>
2018-08-07 13:36:40 -0400

    Another modification attempt

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -170,7 +170,7 @@ cd "${SITE_PATH}"
 NVM_VERSION=`get_config_value 'nvm.version' 'node'`
 echo -e "Setting Node to version ${NVM_VERSION}"
 export NVM_DIR="/srv/config/nvm"
-[ -s $NVM_DIR/nvm.sh ] && source $NVM_DIR/nvm.sh && nvm install "lts/carbon" && nvm use "lts/carbon"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm install lts/carbon && nvm use lts/carbon
 
 # WPEngine sites user repositories installed at the site root, pull the site repo first
 if [ "wpengine" == "${WP_HOST_TYPE}" ]; then
