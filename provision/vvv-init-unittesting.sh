93b5fb092eb81fa182fcffdf99d568a3ed32fa9e
Ryan Leeson <rleeson@trellist.com>
2018-08-14 14:11:46 -0400

    Try as noroot to avoid root caching issue with NVM

--- provision/vvv-init-unittesting.sh
+++ provision/vvv-init-unittesting.sh
@@ -12,7 +12,7 @@ git_repository_pull "${SITE_PATH}" "${DEVELOP_GIT}"
 # Setup NPM build dependencies
 cd "${SITE_PATH}"
 echo -e "NPM install with version ${NVM_VERSION}, this may take a few minutes..."
-nvm exec ${NVM_VERSION} npm install --verbose
+noroot nvm exec ${NVM_VERSION} npm install --verbose
 echo "NPM install done"
 
 if [[ ! -f "${SITE_PATH}/wp-config.php" ]]; then
